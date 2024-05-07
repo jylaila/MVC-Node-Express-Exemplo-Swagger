9a1e63c4727e3cbfc4d906390a8e5fb05254362e
Maria Janaina Silva Ferreira <Maria.Ferreira@fit-tecnologia.org.br>
2024-05-07 15:07:42 -0300

    adjusting variable name

--- models/LoginModel.js
+++ models/LoginModel.js
@@ -8,13 +8,13 @@ module.exports = class LoginModel {
     }
 }
 
-const alunos = [
+const users = [
     {
         'nome': 'Daniela',
         'idade': 17
     },
     {
-        'nome': 'Jõao',
+        'nome': 'João',
         'idade': 21,
     },
     {
